18ee998e36a02af510394756e2b953221ba4f341
PenguinWarz <penguinwarssc2@gmail.com>
2015-07-19 16:36:35 -0600

    Forgot to commit this file.

---
+++ Common/WhiteboardClientData.cpp
@@ -0,0 +1,5 @@
+#include "WhiteboardClientData.h"
+
+const size_t WBClientData::UNCHANGEDCOLOR = UCHAR_MAX;
+const float WBClientData::MINBRUSHSIZE = 1.0f;
+const float WBClientData::MAXBRUSHSIZE = 40.0f;
