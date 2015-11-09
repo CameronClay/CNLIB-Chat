ccdaaece20637bf4f2440e7d24850298a01b2d7e
Cameron Clay <cameronpclay@gmail.com>
2015-11-08 22:42:55 -0700

    Forgot to commit these.

---
+++ Common/CNLIB/CompressionTypes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum CompressionType
+{
+	BESTFIT,		//Chooses best choice for either compression you set or no compression
+	SETCOMPRESSION, //Uses compression you set
+	NOCOMPRESSION   //No compression
+};
