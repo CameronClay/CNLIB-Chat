9cea97d4ea42253dd1a78f45f03b71def89788cc
Cameron Clay <cameronpclay@gmail.com>
2015-11-21 22:20:56 -0700

    Changes to MsgStream.

--- TCPCS/DLLSRC/MsgStream.cpp
+++ TCPCS/DLLSRC/MsgStream.cpp
@@ -1,23 +1,23 @@
 #include "CNLIB\MsgStream.h"
 
-void MsgStreamWriter::Helper<std::string>::Write(const std::string& t)
+template<> void MsgStreamWriter::Helper<std::string>::Write(const std::string& t)
 {
 	const UINT len = (t.size() + 1);
 	((Helper<UINT>*)this)->Write(len);
 	((Helper<const char>*)this)->Write(t.c_str(), len * sizeof(char));
 }
-void MsgStreamWriter::Helper<std::wstring>::Write(const std::wstring& t)
+template<> void MsgStreamWriter::Helper<std::wstring>::Write(const std::wstring& t)
 {
 	const UINT len = (t.size() + 1);
 	((Helper<UINT>*)this)->Write(len);
 	((Helper<const wchar_t>*)this)->Write(t.c_str(), len * sizeof(wchar_t));
 }
 
-std::string& MsgStreamReader::Helper<std::string>::Read()
+template<> std::string& MsgStreamReader::Helper<std::string>::Read()
 {
 	return ((Helper<char>*)this)->Read(((Helper<UINT>*)this)->Read());
 }
-std::wstring& MsgStreamReader::Helper<std::wstring>::Read()
+template<> std::wstring& MsgStreamReader::Helper<std::wstring>::Read()
 {
 	return ((Helper<wchar_t>*)this)->Read(((Helper<UINT>*)this)->Read());
 }
